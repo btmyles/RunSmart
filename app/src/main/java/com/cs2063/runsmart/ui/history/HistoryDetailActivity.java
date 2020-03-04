2c02535e2d2ee6ef3272a7590b1b082fbaedb028
CarterMacLennan <60050873+CarterMacLennan@users.noreply.github.com>
2020-03-04 11:30:22 -0400

    Added are you sure pop up
    Delete Button

--- app/src/main/java/com/cs2063/runsmart/ui/history/HistoryDetailActivity.java
+++ app/src/main/java/com/cs2063/runsmart/ui/history/HistoryDetailActivity.java
@@ -1,15 +1,19 @@
 package com.cs2063.runsmart.ui.history;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cs2063.runsmart.LineLayerActivity;
+import com.cs2063.runsmart.MainActivity;
 import com.cs2063.runsmart.R;
 import com.cs2063.runsmart.util.JsonUtils;
 
@@ -64,8 +68,34 @@ public class HistoryDetailActivity extends AppCompatActivity {
             }
         });
 
+        ImageButton deleteButton = findViewById(R.id.button_delete);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which){
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    //Yes button clicked
+                                    break;
+
+                                case DialogInterface.BUTTON_NEGATIVE:
+                                    //No button clicked
+                                    break;
+                            }
+                        }
+                    };
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                    builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                            .setNegativeButton("No", dialogClickListener).show();
+                }
+        });
+
+
         // This should be
-        getSupportActionBar().setTitle(Long.toString(startTime));
+        getSupportActionBar().setTitle((formatTime(startTime)));
     }
     String formatDuration(long duration) {
         long second = (duration / 1000) % 60;
