7e9d08f3d92d7e32a4eaeb70c74d2c4df009018f
piichuu <46632626+piichuu@users.noreply.github.com>
2020-04-04 20:51:24 -0300

    Revert "Made another change lol"
    This reverts commit da659735cf847dba35898c0801d2477c53a8b766.

--- app/src/main/java/com/cs2063/runsmart/ui/history/HistoryDetailActivity.java
+++ app/src/main/java/com/cs2063/runsmart/ui/history/HistoryDetailActivity.java
@@ -56,7 +56,7 @@ public class HistoryDetailActivity extends AppCompatActivity {
         TextView textDistance = findViewById(R.id.value_distance);
         textDistance.setText(fmt.format(distance)+" km");
         TextView textAvgPace = findViewById(R.id.value_avg_pace);
-        textAvgPace.setText(formatAvgPace((long)avg_pace,distance)+"/km");
+        textAvgPace.setText(formatAvgPace((long)avg_pace)+"/km");
         final TextView textNotes = findViewById(R.id.edit_text);
         textNotes.setText(notes);
 
@@ -136,13 +136,10 @@ public class HistoryDetailActivity extends AppCompatActivity {
         return String.format("%02d:%02d:%02d", hour, minute, second);
     }
 
-    String formatAvgPace(long pace,double distance) {
+    String formatAvgPace(long pace) {
         long second = (pace / 1000) % 60;
         long minute = (pace / (1000 * 60)) % 60;
-        if (distance==0)
-            return "00:00";
-        else
-            return String.format("%02d:%02d", minute, second);
+        return String.format("%02d:%02d", minute, second);
     }
 
     String formatTime(long duration) {
